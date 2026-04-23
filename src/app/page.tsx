2f90a89a2441cf9367d9cae83c036ea6b418759a
malvinaudr <malvinaudriansyah@gmail.com>
2026-04-23 15:35:12 +0700

    chore: moved tech icons and profile icon to icons folder, and changed all image src accordingly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,39 +21,39 @@ interface Skill {
 const skills = [
   {
     name: "HTML",
-    icon: "./html-5.PNG",
+    icon: "icons/html-5.PNG",
   },
   {
     name: "CSS",
-    icon: "./css-3.PNG",
+    icon: "icons/css-3.PNG",
   },
   {
     name: "JavaScript",
-    icon: "./js.PNG",
+    icon: "icons/js.PNG",
   },
   {
     name: "TypeScript",
-    icon: "./ts-logo-512.PNG",
+    icon: "icons/ts-logo-512.PNG",
   },
   {
     name: "React.js",
-    icon: "./react.PNG",
+    icon: "icons/react.PNG",
   },
   {
     name: "Next.js",
-    icon: "./nextjs-icon-light-background.PNG",
+    icon: "icons/nextjs-icon-light-background.PNG",
   },
   {
     name: "Tailwind CSS",
-    icon: "./tailwind.SVG",
+    icon: "icons/tailwind.SVG",
   },
   {
     name: "Git",
-    icon: "./git.PNG",
+    icon: "icons/git.PNG",
   },
   {
     name: "Figma",
-    icon: "./figma.png",
+    icon: "icons/figma.png",
   },
 ];
 
@@ -61,10 +61,10 @@ const categorizedSkills = [
   {
     category: "Languages",
     skills: [
-      { name: "HTML", icon: "./html-5.PNG" },
-      { name: "CSS", icon: "./css-3.PNG" },
-      { name: "JavaScript", icon: "./js.PNG" },
-      { name: "TypeScript", icon: "./ts-logo-512.PNG" },
+      { name: "HTML", icon: "icons/html-5.PNG" },
+      { name: "CSS", icon: "icons/css-3.PNG" },
+      { name: "JavaScript", icon: "icons/js.PNG" },
+      { name: "TypeScript", icon: "icons/ts-logo-512.PNG" },
     ],
   },
   {
@@ -72,15 +72,15 @@ const categorizedSkills = [
     skills: [
       {
         name: "React.js",
-        icon: "./react.PNG",
+        icon: "icons/react.PNG",
       },
       {
         name: "Next.js",
-        icon: "./nextjs-icon-light-background.PNG",
+        icon: "icons/nextjs-icon-light-background.PNG",
       },
       {
         name: "Tailwind CSS",
-        icon: "./tailwind.SVG",
+        icon: "icons/tailwind.SVG",
       },
     ],
   },
@@ -89,19 +89,19 @@ const categorizedSkills = [
     skills: [
       {
         name: "Git",
-        icon: "./git.PNG",
+        icon: "icons/git.PNG",
       },
       {
         name: "VS Code",
-        icon: "./vscode.PNG",
+        icon: "icons/vscode.PNG",
       },
       {
         name: "WordPress",
-        icon: "wordpress.PNG",
+        icon: "icons/wordpress.PNG",
       },
       {
         name: "Elementor",
-        icon: "./Elementor-Logo-Symbol-Red.SVG",
+        icon: "icons/Elementor-Logo-Symbol-Red.SVG",
       },
     ],
   },
@@ -118,7 +118,7 @@ export default function Home() {
           <div className="flex flex-col w-full max-w-full md:flex-row-reverse gap-7 justify-between items-center">
             <div className="bg-slate-300 rounded-full w-full max-w-[400px] aspect-square relative flex justify-center items-center overflow-hidden">
               <Image
-                src={"./profilepic.PNG"}
+                src={"icons/profilepic.PNG"}
                 fill
                 style={{ objectFit: "cover" }}
                 alt="displaypicture"
