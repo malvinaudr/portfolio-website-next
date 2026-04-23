20792942c22611af64fca10618ac7cffb76b8ec3
malvinaudr <malvinaudriansyah@gmail.com>
2026-04-23 13:43:17 +0700

    chore: set the website into static page instead of SSR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
+export const dynamic = "force-static";
 import Image from "next/image";
 import { FaGithub, FaLinkedin, FaPhoneAlt } from "react-icons/fa";
 import { IoMdMail } from "react-icons/io";
